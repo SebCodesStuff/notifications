73d5e2a729b55575532ab5457582356da5a2a97e
Sebastian Namek <30371224+SebCodesStuff@users.noreply.github.com>
2018-12-05 10:20:13 -0500

    updated notification dot (#4)

--- build/NotificationDot.js
+++ build/NotificationDot.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -11,10 +13,27 @@ var _react2 = _interopRequireDefault(_react);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var NotificationDot = function NotificationDot(props) {
-  var show = props.show,
-      style = props.style;
+  var text = props.text,
+      style = props.style,
+      textStyle = props.textStyle,
+      tooltip = props.tooltip;
 
-  return _react2.default.createElement('div', { className: show ? 'show-notification' : 'hide-notification', style: style });
+
+  var getToolTip = {
+    'data-tip': tooltip,
+    'data-place': 'top',
+    'data-type': 'error'
+  };
+
+  return _react2.default.createElement(
+    'div',
+    _extends({ className: 'notification-dot', style: style }, getToolTip),
+    _react2.default.createElement(
+      'p',
+      _extends({ style: textStyle }, getToolTip),
+      text
+    )
+  );
 };
 
 exports.default = NotificationDot;
