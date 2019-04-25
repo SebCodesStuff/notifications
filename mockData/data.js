33737ef6d91921109c6544d6d5afa02515a9973b
snamek <snamek@spinvfx.com>
2019-04-25 18:05:39 -0400

    Landing page demo

---
+++ mockData/data.js
@@ -0,0 +1,13 @@
+export const notifications = [{
+  avatar: null,
+  comments: 'hey',
+  id: 1,
+  name: 'test name',
+  notificationId: 1,
+  received_at: '2018-07-19T15:53:46.896Z',
+  screenshot: 'screenshot_1529357809920',
+  type: 'comment',
+  unviewed: true,
+  user_uuid: 'a5075b70-d219-4d6a-a37c-0add38bb876b',
+  world_uuid: '5c2cc51b-2bfc-4174-8224-127aa3815b1c'
+}]
